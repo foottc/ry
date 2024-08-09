553a69d04fd4cfd439e8cd667b882474c451f41e
FTC团队官方账号 <162798601+foottc@users.noreply.github.com>
2024-08-09 13:18:38 +0800

    Update rainyun.py

--- rainyun.py
+++ rainyun.py
@@ -6,7 +6,7 @@ payload = json.dumps({
    "task_name": "每日签到"
 })
 headers = {
-   'x-api-key': '{此处填入你的api密钥}',
+   'x-api-key': 'UINQdH7bGnA0lIPIBKnVd1pkykjIyW5i',
    'User-Agent': 'Mozilla/5.0',
    'Content-Type': 'application/json'
 }
